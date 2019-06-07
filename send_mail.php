829c5161b99e32584781571572d5a9a12e9ea246
Nitish <Sr.nitish06@gmail.com>
2019-06-07 20:52:04 +0530

    mail

--- send_mail.php
+++ send_mail.php
@@ -5,7 +5,7 @@ $msg = 'First Name: ' .$_POST['first_name'] ."\n"
 		.'Email: ' .$_POST['email'] ."\n"
 		.'Message: ' .$_POST['message'];
 
-		mail('enquiry@vikihealth.ai', 'VIKI Contact Us Form', $msg); 
+		mail('presales@airodigitallabs.com', 'VIKI Contact Us Form', $msg); 
 		// header('location: contact-us-tank-you.php');
 		// alert("Thank You for your interest, we will get back to you soon!!");
 		// echo "<script> alert('Thank You for your interest, we will get back to you soon!!'); </script>"
