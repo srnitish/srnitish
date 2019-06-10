31170490b73e2d93c552470e0d51b85aa3cd48a6
Nitish <Sr.nitish06@gmail.com>
2019-06-10 16:44:29 +0530

    favicon and Meet Viki banner.

--- send_mail.php
+++ send_mail.php
@@ -5,22 +5,18 @@ $msg = 'First Name: ' .$_POST['first_name'] ."\n"
 		.'Email: ' .$_POST['email'] ."\n"
 		.'Message: ' .$_POST['message'];
 
-		if(mail('sr.nitish06@gmail.com', 'VIKI Contact Us Form', $msg)){
-			echo "Thank You for your interest, we will get back to you soon!!";
-			header('location: index.php');
-		}
+		mail('presales@airodigitallabs.com', 'VIKI Contact Us Form', $msg); 
 		// header('location: contact-us-tank-you.php');
 		// alert("Thank You for your interest, we will get back to you soon!!");
 		// echo "<script> alert('Thank You for your interest, we will get back to you soon!!'); </script>"
 		// echo "Thank You for your interest, we will get back to you soon!!";
-		// echo '<script language="javascript">';
-		// echo 'alert("Thank You for your interest, we will get back to you soon!!")';
+		echo '<script language="javascript">';
+		echo 'alert("Thank You for your interest, we will get back to you soon!!")';
 		// echo 'window.location = 'index.php';';
-		// echo '</script>';
-	} 
-	// else
-	// {
-	// 	header('location: index.php');
-	// 	exit(0);
-	// }
+		echo '</script>';
+		header('location: index.php');
+} else{
+	header('location: index.php');
+	exit(0);
+}
 ?>
