a9e3eae5ec410e2d18d23033c6adf8b8c8032e06
Nitish <Sr.nitish06@gmail.com>
2019-06-10 16:01:04 +0530

    final

--- send_mail.php
+++ send_mail.php
@@ -5,18 +5,22 @@ $msg = 'First Name: ' .$_POST['first_name'] ."\n"
 		.'Email: ' .$_POST['email'] ."\n"
 		.'Message: ' .$_POST['message'];
 
-		mail('presales@airodigitallabs.com', 'VIKI Contact Us Form', $msg); 
+		if(mail('sr.nitish06@gmail.com', 'VIKI Contact Us Form', $msg)){
+			echo "Thank You for your interest, we will get back to you soon!!";
+			header('location: index.php');
+		}
 		// header('location: contact-us-tank-you.php');
 		// alert("Thank You for your interest, we will get back to you soon!!");
 		// echo "<script> alert('Thank You for your interest, we will get back to you soon!!'); </script>"
 		// echo "Thank You for your interest, we will get back to you soon!!";
-		echo '<script language="javascript">';
-		echo 'alert("Thank You for your interest, we will get back to you soon!!")';
+		// echo '<script language="javascript">';
+		// echo 'alert("Thank You for your interest, we will get back to you soon!!")';
 		// echo 'window.location = 'index.php';';
-		echo '</script>';
-		header('location: index.php');
-} else{
-	header('location: index.php');
-	exit(0);
-}
+		// echo '</script>';
+	} 
+	// else
+	// {
+	// 	header('location: index.php');
+	// 	exit(0);
+	// }
 ?>
